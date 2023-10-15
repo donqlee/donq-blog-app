f02e2967f6625de3a7861bb91695af1911c70f18
donqlee <donqlee88@gmail.com>
2023-10-15 20:18:35 +0900

    feat: firebase auth 로그아웃

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,20 @@
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "firebaseApp";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export default function Profile() {
+  const auth = getAuth(app);
+  const onSignOut = async () => {
+    try {
+      const auth = getAuth(app);
+      await signOut(auth);
+      toast.success("로그아웃 되었습니다.");
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error?.code);
+    }
+  };
   return (
     <div className="profile__box">
       <div className="flex__box-lg">
@@ -10,7 +24,7 @@ export default function Profile() {
           <div className="profile__name">김유저</div>
         </div>
       </div>
-      <Link to="/" className="profile__logout">
+      <Link to="/" className="profile__logout" onClick={onSignOut}>
         로그아웃
       </Link>
     </div>
