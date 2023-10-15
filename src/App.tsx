619cd24b524a1bc8daf45fb1a47f1f5562430bab
donqlee <donqlee88@gmail.com>
2023-10-15 20:04:15 +0900

    feat: firebase auth 로그인

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { app } from "firebaseApp";
-import { getAuth } from "firebase/auth";
-import Router from "./components/Router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import Router from "./components/Router";
+import Loader from "components/Loader";
+
 function App() {
   const auth = getAuth(app);
+  const [init, setInit] = useState<boolean>(false);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   );
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });
+  }, [auth]);
+
+  useEffect(() => {});
   return (
     <>
       <ToastContainer />
-      <Router isAuthenticated={isAuthenticated} />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </>
   );
 }
